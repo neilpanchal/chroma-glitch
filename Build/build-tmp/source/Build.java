30e7b7cba57ef5300bff27fbd3e7ea1ad1384d1f
Neil Panchal <email@neil.engineer>
2015-10-24 19:24:58 -0700

    Added PDF Format

--- Build/build-tmp/source/Build.java
+++ Build/build-tmp/source/Build.java
@@ -1,14 +1,35 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import com.chroma.*; 
+import com.luma.*; 
+import java.util.UUID; 
+import java.util.ArrayList; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Build extends PApplet {
+
 // Author: Neil Panchal
 // License: The MIT License
 // Copyright (c) 2015 Neil Panchal, http://neil.engineer
 // ----------------------------------------------------------------------------
 
 // Import libraries
-import com.chroma.*;
-import com.luma.*;
 
-import java.util.UUID;
-import java.util.ArrayList;
+
+
+
+
 
 // Project details
 String project_name = "BoilerPlate";
@@ -26,17 +47,17 @@ boolean savePDF = false;
 Chroma[] palette = generatePalette().getClusters();
 
 // ----------------------------------------------------------------------------
-void setup() {
+public void setup() {
 
-    size(650, 950);
-    pixelDensity(2);
+    
+    
     background(getRandomColor().get());
     frameRate(24);
-    smooth();
+    
 
 }
 
-void draw() {
+public void draw() {
 
     // Begin Draw
     delay(0);
@@ -62,11 +83,11 @@ void draw() {
 
 
 // ----------------------------------------------------------------------------
-void mousePressed() {
+public void mousePressed() {
    // Reset function
 }
 
-void keyReleased() {
+public void keyReleased() {
     // Save a screenshot in PNG format
     if (key == 's' || key == 'S') {
         saveFrame("../Export/" + project_name + "_" + frame + "_" + UUID.randomUUID().toString().substring(0, 8) +  ".png");
@@ -78,7 +99,7 @@ void keyReleased() {
 }
 
 // Generate Palette
-Luma generatePalette() {
+public Luma generatePalette() {
     // Color palette
     int lumaNumber = 8, lumaQuality = 70;
     int lumaMinL = 15, lumaMaxL = 25;
@@ -93,11 +114,21 @@ Luma generatePalette() {
 }
 
 // Get a random palette color
-Chroma getRandomColor() {
+public Chroma getRandomColor() {
     return (bgWhite) ? new Chroma(255) : palette[(int)random(0, palette.length)];
 }
 
-void delay(int delay) {
+public void delay(int delay) {
     int time = millis();
     while (millis() - time <= delay);
+}
+  public void settings() {  size(650, 950);  smooth();  pixelDensity(2); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
